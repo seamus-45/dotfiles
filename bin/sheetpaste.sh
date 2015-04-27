22f3d539d4a6828c935bf65bd570ac85dea12066
Федотов Сергей <fedotov_sv@taximaxim.ru>
2015-04-27 17:48:03 +0500

    fix errors

--- bin/sheetpaste.sh
+++ bin/sheetpaste.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 TEMP=$(mktemp)
 
-while read input; do
-  echo $input | tee >${TEMP}
-done
+tee -a >${TEMP}
 
 if [ -s ${TEMP} ]; then
   curl -s -i -X POST --data-urlencode content@${TEMP} --data-urlencode age='+3 month' http://localhost/api/post | tail -n1
@@ -11,3 +9,5 @@ else
   echo "Paste is empty!"
   exit 1
 fi
+
+rm -f ${TEMP}
