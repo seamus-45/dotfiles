f5754515d34a3929735fc6dc9ad6ec11a7b20f55
Федотов Сергей <fedotov_sv@taximaxim.ru>
2015-05-05 15:05:34 +0500

    fix address

--- bin/sheetpaste.sh
+++ bin/sheetpaste.sh
@@ -4,7 +4,7 @@ TEMP=$(mktemp)
 tee -a >${TEMP}
 
 if [ -s ${TEMP} ]; then
-  curl -s -i -X POST --data-urlencode content@${TEMP} --data-urlencode age='+3 month' http://localhost/api/post | tail -n1
+  curl -s -i -X POST --data-urlencode content@${TEMP} --data-urlencode age='+3 month' http://10.145.13.84/api/post | tail -n1
 else
   echo "Paste is empty!"
   exit 1
