821c47b999f82eca716a09def6c1135825b6aaa1
Федотов Сергей <fedotov_sv@taximaxim.ru>
2015-05-21 11:31:56 +0500

    bump

--- bin/xswitchscreen.sh
+++ bin/xswitchscreen.sh
@@ -3,8 +3,8 @@ eval $(xdotool getmouselocation --shell)
 
 if [[ ${X} -lt 1680 ]]; then
   xdotool mousemove 1900 1000
-  echo '*' | osd_cat -p top -o 48 -i 3260 -d 1 -c red -f -misc-fixed-medium-r-normal--60-0-100-100-c-0-koi8-r -s 1
+  echo '*' | osd_cat -p bottom -i 1700 -d 1 -c red -f -misc-fixed-medium-r-normal--60-0-100-100-c-0-koi8-r -s 1
 else
-  xdotool mousemove 220 1000
-  echo '*' | osd_cat -p top -o 48 -i 1600 -d 1 -c red -f -misc-fixed-medium-r-normal--60-0-100-100-c-0-koi8-r -s 1
+  xdotool mousemove 1420 1000
+  echo '*' | osd_cat -p bottom -i 1600 -d 1 -c red -f -misc-fixed-medium-r-normal--60-0-100-100-c-0-koi8-r -s 1
 fi
