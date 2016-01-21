ae57d6d8f5b1304e1dcb6adf857a98c59fdd169a
seamus <sr.fido@gmail.com>
2016-01-21 11:22:10 +0500

    bump

--- tmp/ubuntu-scratch/scripts/pack.sh
+++ tmp/ubuntu-scratch/scripts/pack.sh
@@ -2,5 +2,17 @@
 ./pack-rootfs.sh && \
 ./pack-iso.sh || exit 1
 SIZE=$(du -csh ubuntu-remix.iso | head -n1 | cut -f1)
-#mv -v ubuntu-remix.iso /home/fedotov_sv/downloads/iso/ubuntu/ubuntu-14.04-i686-taxi-$(date +%Y%m%d).iso
+REL=$(chroot chroot lsb_release -rs)
+ARCH=$(chroot chroot arch)
+
+read -n1 -p "Move to ISO folder? (y,n)" choice
+echo -ne '\n'
+case $choice in
+	[yY])
+		mv -v ubuntu-remix.iso /home/fedotov_sv/downloads/iso/ubuntu/ubuntu-${REL}-${ARCH}-taxi-$(date +%Y%m%d).iso
+		;;
+	*)
+		;;
+esac
+
 echo "Size: $SIZE"
