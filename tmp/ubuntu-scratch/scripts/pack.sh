aae8a5ea1e3c3429e18675a7d94c38d2ace3d5e4
seamus <sr.fido@gmail.com>
2016-01-26 14:57:36 +0500

    fix mistype

--- tmp/ubuntu-scratch/scripts/pack.sh
+++ tmp/ubuntu-scratch/scripts/pack.sh
@@ -10,7 +10,7 @@ echo -ne '\n'
 case $choice in
 	[yY])
 		mv -v ubuntu-remix.iso /home/fedotov_sv/downloads/iso/ubuntu/ubuntu-${REL}-${ARCH}-taxi-$(date +%Y%m%d).iso
-		ln -sf ubuntu-${REL}-${ARCH}-taxi-$(date +%Y%m%d).iso ubuntu-${REL}-${ARCH}-taxi-latest.iso
+		ln -sf ubuntu-${REL}-${ARCH}-taxi-$(date +%Y%m%d).iso /home/fedotov_sv/downloads/iso/ubuntu/ubuntu-${REL}-${ARCH}-taxi-latest.iso
 		;;
 	*)
 		;;
