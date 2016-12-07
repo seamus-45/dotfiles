af48944482f05d2728163aef7a46e8dd434c4101
Федотов Сергей <fedotov_sv@taximaxim.ru>
2016-12-07 11:21:44 +0500

    bump

--- music/Music For Programmers/mfp_clone.sh
+++ music/Music For Programmers/mfp_clone.sh
@@ -1,1 +1,1 @@
-wget -O - musicforprogramming.net/rss.php 2>/dev/null | grep enclosure | awk '{print $2}' | sed 's@^url="\(.*\)"$@\1@g' | while read ff; do wget -c --limit-rate=500k $ff; done
+wget -O - musicforprogramming.net/rss.php 2>/dev/null | grep enclosure | awk '{print $2}' | sed 's@^url="\(.*\)"$@\1@g' | while read ff; do wget -c --limit-rate=1500k $ff; done
