69375b1d267625b8026e2e9f6b8b9a998fade78d
seamus-45 <fedotov_sv@taximaxim.ru>
2017-05-24 16:20:47 +0500

    bump

--- bin/bspc_shot_all_desktops.sh
+++ bin/bspc_shot_all_desktops.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-DOFFSET=$(( $(bspc query -D | wc -l) / 2 ))
-N=4
+DOFFSET=6
+N=6
 i=1
 IN=''
 while [[ $i -le $N ]];
 do
   bspc desktop -f \^$i; 
   bspc desktop -f \^$(( $i+$DOFFSET )); 
-  sleep 0.5;
-  scrot -m $TMPDIR/${i}.png;
-  IN="$IN $TMPDIR/${i}.png"
+  sleep 1;
+  maim ${i}.png;
+  IN="$IN ${i}.png"
   i=$(( $i + 1 ))
 done
-convert -append $IN scrot.png
+convert -append $IN bspwm.png
 rm -v $IN
