129b2508f6781460415682db3a057ac7272155b8
seamus-45 <fedotov_sv@taximaxim.ru>
2017-06-05 13:31:07 +0500

    modify for viewtube script

--- bin/playtube.sh
+++ bin/playtube.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-yellow='\e[1;33m'
-nc='\e[0m'
-ps="${yellow}>${nc}"
+if [ $# -lt 1 ]; then
+  echo "Usage: $(basename $0) URL"
+  exit 1
+fi
+
+URL=$1
+
+if [[ ${URL} =~ ^viewtube ]];
+then
+  URL=$(echo ${URL} | sed -e "s/viewtube://")
+fi
 
 if [ ! -x /usr/bin/youtube-dl ]; then
   echo 'You must install youtube-dl for parse URI media content.'
   exit 1
 fi
 
-if [ $# -lt 1 ]; then
-  echo 'Paste URL here and press <enter> to play.'
-  echo 'Leave blank and press <enter> to quit.'
-  while echo -ne ${ps}; read URI; do
-    [[ -z ${URI} ]] && exit 0
-    mpv -geometry 500x300-50-60 -really-quiet $(youtube-dl --get-url "${URI}") &
-  done
-else
-    mpv -geometry 500x300-50-60 -really-quiet $(youtube-dl --get-url "${1}") &
-fi
+mpv -geometry 500x300-50-60 -really-quiet $(youtube-dl --get-url "${URL}") &
