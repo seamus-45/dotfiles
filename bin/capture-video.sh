3890ea6e7aa7383dd165b9bb10de739a072e8d55
seamus-45 <fedotov_sv@taximaxim.ru>
2017-09-14 11:52:13 +0500

    bump

--- bin/capture-video.sh
+++ bin/capture-video.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i")
-ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y ~/myfile.webm
+ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y ~/capture.mp4
